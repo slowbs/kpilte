7fe81bbd1a7d919504fdf6f972b295028b69b589
slowbs <poison_word@hotmail.com>
2019-12-09 08:59:27 +0700

    shared component

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SidebarComponent, ControlSidebarComponent, FooterComponent],
   imports: [
     CommonModule
   ]
