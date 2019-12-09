f767f4b0ce2d838260fa50826f21da158e3575f8
slowbs <poison_word@hotmail.com>
2019-12-09 08:50:55 +0700

    kpilte but need to refact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { IndexComponent } from './components/content/index/index.component';
+import { ContentComponent } from './components/shared/content/content.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
