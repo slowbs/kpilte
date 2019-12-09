8fb414777e7fd8a690794f9a1412bbd62eee4c15
slowbs <poison_word@hotmail.com>
2019-12-09 14:38:25 +0700

    router work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/content/index/index.component';
 import { SharedModule } from './components/shared/shared.module';
 import { LoginComponent } from './components/content/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -17,7 +17,7 @@ import { LoginComponent } from './components/content/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
