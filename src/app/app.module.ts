8f21241528dd84f58b5964c2b027cfa6a4a3ee01
slowbs <poison_word@hotmail.com>
2019-12-11 16:00:48 +0700

    before backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/content/index/index.component';
@@ -21,7 +23,9 @@ import { DataTablesModule } from 'angular-datatables';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    DataTablesModule
+    DataTablesModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/kpi.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { backendURL } from '../app.url'
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KpiService {
+
+  
+
+  private backendAPI: string = environment.production ?
+        // 'http://61.19.202.217/modbudget/backend/api' :
+        'http://192.168.5.104/modbudget/backend/api' :
+        `${backendURL}/api/`;
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+}
