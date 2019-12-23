d57464243bd0ba0f8c01b4ce4c2ed5026a8ef195
slowbs <poison_word@hotmail.com>
2019-12-23 15:45:21 +0700

    profile api and dropdown logout

--- backend/login/profile.php
+++ backend/login/profile.php
@@ -3,7 +3,7 @@
 if ( isset($_SESSION['login']) && !empty($_SESSION['login']) )
 {
     $id = $_SESSION['login'];
-    $query = mysqli_query($database, "SELECT * from users WHERE id = $id");
+    $query = mysqli_query($database, "SELECT username, user_type, hospcode from users WHERE id = $id");
     $result = mysqli_fetch_assoc($query);
     exit(json_encode($result));
     // exit(json_encode($id));
